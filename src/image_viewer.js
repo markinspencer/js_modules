c3c236cd65c1212cca21281b9d85b83fa52ddf4f
Spencer Markin <smarkin@questica.com>
2019-03-20 09:52:28 -0400

    added webpack configuration for images

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,13 @@
 import "../styles/image_viewer.css";
+import big from "../assests/big.jpg";
+import small from "../assests/small.jpg";
 
 const image = document.createElement("img");
-image.src = "http://lorempixel.com/400/400";
+image.src = small;
 
 document.body.appendChild(image);
+
+const bigImage = document.createElement("img");
+bigImage.src = big;
+
+document.body.appendChild(bigImage);

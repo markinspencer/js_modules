6238b1b03fbbbbea67c7fbf9bdd582212d1b50ef
Spencer Markin <smarkin@questica.com>
2019-04-12 12:54:13 -0400

    finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const button = document.createElement("button");
-
 button.innerText = "Click me";
 button.onclick = () => {
   System.import("./image_viewer").then(module => {
-    console.log(module);
+    module.default();
   });
 };
 
